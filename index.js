e0751aa0678cfdfa50be38517a58f0cb57db9544
Mike Shine <michaelbshine@outlook.com>
2021-02-02 14:19:48 -0800

    changed which elements appear on calc display and flex wrapped two inner containers

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ import {add, subtract, multiply, divide} from './operations.js';
 const body = document.querySelector('body');
 
 const largeContainer = document.createElement('div');
+largeContainer.classList.add('largeContainer');
 body.appendChild(largeContainer);
 
 const displayContainer = document.createElement('div');
+displayContainer.classList.add('displayContainer');
 largeContainer.appendChild(displayContainer);
 
 const buttonContainer = document.createElement('div');
+buttonContainer.classList.add('buttonContainer');
 largeContainer.appendChild(buttonContainer);
 
 const lineBreak = document.createElement('br');
+
+
+
+
 
 
 /* Numerical buttons appended to the DOM */
@@ -21,6 +28,7 @@ const lineBreak = document.createElement('br');
 const number7Button = document.createElement('button');
 number7Button.textContent = '7';
 number7Button.classList.add('numberButton');
+number7Button.classList.add('rowTwo');
 number7Button.addEventListener('click', () => {
   assign('7')
   display.textContent = displayString;
@@ -33,6 +41,7 @@ buttonContainer.appendChild(number7Button);
 const number8Button = document.createElement('button');
 number8Button.textContent = '8';
 number8Button.classList.add('numberButton');
+number8Button.classList.add('rowTwo');
 number8Button.addEventListener('click', () => {
   assign('8')
   display.textContent = displayString;
@@ -45,6 +54,7 @@ buttonContainer.appendChild(number8Button);
 const number9Button = document.createElement('button');
 number9Button.textContent = '9';
 number9Button.classList.add('numberButton');
+number9Button.classList.add('rowTwo');
 number9Button.addEventListener('click', () => {
   assign('9')
   display.textContent = displayString;
@@ -58,6 +68,7 @@ buttonContainer.appendChild(lineBreak);
 const number4Button = document.createElement('button');
 number4Button.textContent = '4';
 number4Button.classList.add('numberButton');
+number4Button.classList.add('rowThree');
 number4Button.addEventListener('click', () => {
   assign('4')
   display.textContent = displayString;
@@ -70,6 +81,7 @@ buttonContainer.appendChild(number4Button);
 const number5Button = document.createElement('button');
 number5Button.textContent = '5';
 number5Button.classList.add('numberButton');
+number5Button.classList.add('rowThree');
 number5Button.addEventListener('click', () => {
   assign('5')
   display.textContent = displayString;
@@ -82,6 +94,7 @@ buttonContainer.appendChild(number5Button);
 const number6Button = document.createElement('button');
 number6Button.textContent = '6';
 number6Button.classList.add('numberButton');
+number6Button.classList.add('rowThree');
 number6Button.addEventListener('click', () => {
   assign('6')
   display.textContent = displayString;
@@ -95,6 +108,7 @@ buttonContainer.appendChild(lineBreak);
 const number1Button = document.createElement('button');
 number1Button.textContent = '1';
 number1Button.classList.add('numberButton');
+number1Button.classList.add('rowFour');
 number1Button.addEventListener('click', () => {
   assign('1')
   display.textContent = displayString;
@@ -107,6 +121,7 @@ buttonContainer.appendChild(number1Button);
 const number2Button = document.createElement('button');
 number2Button.textContent = '2';
 number2Button.classList.add('numberButton');
+number2Button.classList.add('rowFour');
 number2Button.addEventListener('click', () => {
   assign('2')
   display.textContent = displayString;
@@ -119,6 +134,7 @@ buttonContainer.appendChild(number2Button);
 const number3Button = document.createElement('button');
 number3Button.textContent = '3';
 number3Button.classList.add('numberButton');
+number3Button.classList.add('rowFour');
 number3Button.addEventListener('click', () => {
   assign('3')
   display.textContent = displayString;
@@ -127,6 +143,23 @@ number3Button.addEventListener('click', () => {
 })
 buttonContainer.appendChild(number3Button);
 
+const number0Button = document.createElement('button');
+number0Button.textContent = '0';
+number0Button.classList.add('numberButton');
+number0Button.classList.add('rowFive');
+number0Button.addEventListener('click', () => {
+  assign('0')
+  display.textContent = displayString;
+  console.log('operand1: ', operand1)
+  console.log('operand2: ', operand2)
+})
+buttonContainer.appendChild(number0Button);
+
+
+
+
+
+
 
 
 /* Operations buttons appended to DOM */
@@ -134,6 +167,7 @@ buttonContainer.appendChild(number3Button);
 const plusButton = document.createElement('button');
 plusButton.textContent = '+';
 plusButton.classList.add('operationButton');
+plusButton.classList.add('rowFour');
 plusButton.addEventListener('click', () => {
   operate(add);
   display.textContent = displayString;
@@ -144,6 +178,7 @@ buttonContainer.appendChild(plusButton);
 const minusButton = document.createElement('button');
 minusButton.textContent = '-';
 minusButton.classList.add('operationButton');
+minusButton.classList.add('rowThree');
 minusButton.addEventListener('click', () => {
   operate(subtract);
   display.textContent = displayString;
@@ -154,6 +189,7 @@ buttonContainer.appendChild(minusButton);
 const multiplyButton = document.createElement('button');
 multiplyButton.textContent = 'x';
 multiplyButton.classList.add('operationButton');
+multiplyButton.classList.add('rowTwo');
 multiplyButton.addEventListener('click', () => {
   operate(multiply);
   display.textContent = displayString;
@@ -164,6 +200,7 @@ buttonContainer.appendChild(multiplyButton);
 const divideButton = document.createElement('button');
 divideButton.textContent = '÷';
 divideButton.classList.add('operationButton');
+divideButton.classList.add('rowOne');
 divideButton.addEventListener('click', () => {
   operate(divide);
   display.textContent = displayString;
@@ -174,6 +211,7 @@ buttonContainer.appendChild(divideButton);
 const clearButton = document.createElement('button');
 clearButton.textContent = 'C';
 clearButton.classList.add('clearButton');
+clearButton.classList.add('rowOne');
 clearButton.addEventListener('click', () => {
   clear();
   display.textContent = displayString;
@@ -183,11 +221,16 @@ buttonContainer.appendChild(clearButton);
 const equalsButton = document.createElement('button');
 equalsButton.textContent = '=';
 equalsButton.classList.add('equalsButton');
+equalsButton.classList.add('rowFive');
 equalsButton.addEventListener('click', () => {
   evaluate(operand1, operand2, operator);
   display.textContent = displayString;
 })
 buttonContainer.appendChild(equalsButton);
+
+
+
+
 
 
 
@@ -205,6 +248,7 @@ function assign(inputString) {
     console.log('displayString: ', displayString)
   } else if (operand1 && !operand2) {
     operand2 = inputString;
+    displayString = '';
     displayString += operand2;
     console.log('displayString: ', displayString)
   }
@@ -214,20 +258,6 @@ function operate(inputFunc) {
   if (!operator) {
     operator = inputFunc;
   }
-  if (inputFunc.name === 'add') {
-    displayString += ' + ';
-    console.log('displayString: ', displayString)
-  } else if (inputFunc.name === 'subtract') {
-    displayString += ' - ';
-    console.log('displayString: ', displayString)
-  } else if (inputFunc.name === 'multiply') {
-    displayString += ' x ';
-    console.log('displayString: ', displayString)
-  } else if (inputFunc.name === 'divide') {
-    displayString += ' ÷ ';
-    console.log('displayString: ', displayString)
-  }
-
 }
 
 function evaluate(input1, input2, mathOperation) {
@@ -254,6 +284,10 @@ function clear() {
 
 
 
+
+
+
+
 /* Display functionality */
 
 let displayString = ''
@@ -268,9 +302,20 @@ displayContainer.appendChild(lineBreak);
 
 
 
+
+
+
+
+
+
 /* Notes
 
 flexbox tasks:
+
+ - center display and buttons (indiv. containers, or largeContainer?)
+ - decide on width of row
+ - Add all buttons first?
+
  - only three buttons per row
  - adjust size of equals button as needed
  - adjust width of display as needed
