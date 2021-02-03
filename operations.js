2c4140e55cce1473298c2f13f0756550f027a563
Mike Shine <michaelbshine@outlook.com>
2021-02-03 14:54:56 -0800

    changed from 2 decimal places rounding to 3

--- operations.js
+++ operations.js
@@ -1,21 +1,21 @@
 function add(num1, num2) {
   name: 'add';
-  return +(num1 + num2).toFixed(2);
+  return +(num1 + num2).toFixed(3);
 }
 
 function subtract(num1, num2) {
   name: 'subtract';
-  return +(num1 - num2).toFixed(2);
+  return +(num1 - num2).toFixed(3);
 }
 
 function multiply(num1, num2) {
   name: 'multiply';
-  return +(num1 * num2).toFixed(2);
+  return +(num1 * num2).toFixed(3);
 }
 
 function divide(num1, num2) {
   name: 'divide';
-  return +(num1 / num2).toFixed(2);
+  return +(num1 / num2).toFixed(3);
 }
 
 
